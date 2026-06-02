00b07b11625a85391bc7011f679850f3df50f4a2
sk7-dev <karthee.shiv@gmail.com>
2026-06-01 20:10:32 -0700

    Py 07

---
+++ SQL 50/Python/07_1068_product_sales_analysis_I.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+def sales_analysis(sales: pd.DataFrame, product: pd.DataFrame) -> pd.DataFrame:
+    merged = sales.merge(product, on="product_id", how="left")
+    return merged[["product_name", "year", "price"]]
