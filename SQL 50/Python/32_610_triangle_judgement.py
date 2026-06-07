f6f8cc31f94b4ae447b75d5785043371c3904e87
sk7-dev <karthee.shiv@gmail.com>
2026-06-06 20:11:45 -0700

    Py 32

---
+++ SQL 50/Python/32_610_triangle_judgement.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+def triangle_judgement(triangle: pd.DataFrame) -> pd.DataFrame:
+    triangle["triangle"] = triangle.apply(
+        lambda row: "Yes"
+        if row["x"] + row["y"] > row["z"]
+        and row["x"] + row["z"] > row["y"]
+        and row["y"] + row["z"] > row["x"]
+        else "No",
+        axis=1
+    )
+
+    return triangle
