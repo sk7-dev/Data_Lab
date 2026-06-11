7cf41b93b08024a5d32c91ca6835fd8d82f72398
sk7-dev <karthee.shiv@gmail.com>
2026-06-10 20:15:16 -0700

    Py 48

---
+++ SQL 50/Python/48_1484_group_sold_products_by_the_date.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+    grouped = activities.groupby('sell_date')['product'].agg([
+        ('num_sold', 'nunique'),
+        ('products', lambda x: ','.join(sorted(x.unique())))
+    ]).reset_index()
+    return grouped
