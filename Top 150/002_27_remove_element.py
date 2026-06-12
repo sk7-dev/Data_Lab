3546ee86c94f9616b3c0820580d911ecfdbdebc5
sk7-dev <karthee.shiv@gmail.com>
2026-06-11 19:52:06 -0700

    Py 002 changes

--- Top 150/002_27_remove_element.py
+++ Top 150/002_27_remove_element.py
@@ -1,13 +1,8 @@
-class Solution {
-public:
-    int removeElement(vector<int>& nums, int val) {
-        int k = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] != val) {
-                nums[k] = nums[i];
-                k++;
-            }
-        }
-        return k;
-    }
-};
+class Solution(object):
+    def removeElement(self, nums, val):
+        k = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i]
+                k += 1
+        return k
