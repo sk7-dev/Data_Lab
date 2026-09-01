1e08a7d40710191db787187283767b002337cd12
sk7-dev <karthee.shiv@gmail.com>
2026-08-31 22:06:54 -0700

    Py 083

---
+++ Top 150/083_102_binary_tree_level_order_traversal.py
@@ -0,0 +1,19 @@
+from collections import deque
+from typing import List, Optional
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        result = []
+        queue = deque([root])
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+        return result
